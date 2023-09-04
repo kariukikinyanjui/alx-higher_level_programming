a3a17d37fa52c3d1397d75e89b3736fb77df8c9c
kariukikinyanjui <michaelkariuki7@gmail.com>
2023-09-04 07:52:04 +0000

    Simple rectangle

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Define a class Rectangle."""
+
+
+class Rectangle:
+    """A class representing a rectangle."""
+
+    def __init__(self):
+        """Initializes a Rectangle instance with the given width and height."""
+        pass
