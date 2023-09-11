873c88ee3d65fcd42b50fb24eab9deab8922fa58
kariukikinyanjui <michaelkariuki7@gmail.com>
2023-09-11 16:45:35 +0000

    A class Rectangle that inherits from BaseGeometry

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""Define a class BaseGeometry"""
+
+
+class BaseGeometry:
+    """A base class for geometric shapes."""
+
+    def area(self):
+        """Calculate the area of the geometric shape"""
+
+        raise Exception("area() is not implemeted")
+
+    def integer_validator(self, name, value):
+        """Validate if a value is a positive integer"""
+
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
+
+
+class Rectangle(BaseGeometry):
+    """A class representing a rectangle, inheriting from BaseGeometry"""
+
+    def __init__(self, width, height):
+        """Initialize a Rectangle instance with width and height"""
+
+        self.__width = 0
+        self.__height = 0
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        """Calculate the area of the rectangle"""
+        return self.__width * self.__height
+
+    def __str__(self):
+        """Return a string representation of the rectangle"""
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
