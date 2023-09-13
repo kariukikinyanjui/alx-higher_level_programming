4b4f95876c663262b7792f490171e1b11d8770db
kariukikinyanjui <michaelkariuki7@gmail.com>
2023-09-13 22:44:01 +0000

    Fixed bug

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -14,15 +14,4 @@ def class_to_json(obj):
         dict: A dictionary representation of the object for JSON serialization
     """
 
-    if not hasattr(obj, '__dict__'):
-        raise ValueError("Input object is not serializable")
-
-    result = {}
-    for key, value in obj.__dict__.items():
-        if isinstance(value, (int, str, bool)):
-            result[key] = value
-
-        elif isinstance(value, (list, dict)):
-            result[key] = class_to_json(value)
-
-    return result
+    return obj.__dict__
