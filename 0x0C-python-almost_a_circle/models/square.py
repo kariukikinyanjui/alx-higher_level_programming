250c0cbbba30593cfdbbe7999ed11dce0c0227cb
kariukikinyanjui <michaelkariuki7@gmail.com>
2023-09-18 14:56:46 +0000

    Added public method def update(self, *args, **kwargs) that assighs attributes

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -41,3 +41,24 @@ class Square(Rectangle):
             str: String representation of the square.
         """
         return f"[Square] ({self.id}) {self.x}/{self.y} - {self.size}"
+
+    def update(self, *args, **kwargs):
+        """
+        Update the attributes of the square based on the given arguments.
+
+        Args:
+            *args: No-keyworkd arguments in the order (id, size, x, y).
+            **kwargs: Keyworkd arguments representing attribute-value pairs.
+        """
+        if args:
+            if len(args) >= 1:
+                self.id = args[0]
+            if len(args) >= 2:
+                self.size = args[1]
+            if len(args) >= 3:
+                self.x = args[2]
+            if len(args) >= 4:
+                self.y = args[3]
+        else:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
