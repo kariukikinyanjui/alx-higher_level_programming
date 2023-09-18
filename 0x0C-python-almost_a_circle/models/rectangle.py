862b504675c7a6a03a13b74e5c59c0bc504be4a7
kariukikinyanjui <michaelkariuki7@gmail.com>
2023-09-18 14:23:15 +0000

    Added the public method def update(self, *args):

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -122,3 +122,22 @@ class Rectangle(Base):
             str: String representation of the rectangle.
         """
         return f"[Rectangle] ({self.id}) {self.__x}/{self.__y} - {self.__width}/{self.__height}"
+
+    def update(self, *args):
+        """
+        Update the attributes of the rectangle based on the given argumnets.
+
+        Args:
+            *args: No-keyword arguments in the order (id, width, height, x, y)
+        """
+        if args:
+            if len(args) >= 1:
+                self.id = args[0]
+            if len(args) >= 2:
+                self.width = args[1]
+            if len(args) >= 3:
+                self.height = args[2]
+            if len(args) >= 4:
+                self.x = args[3]
+            if len(args) >= 5:
+                self.y = args[4]
