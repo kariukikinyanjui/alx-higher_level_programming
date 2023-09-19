475e0dd922c4b42897fa98194540825b6e4d244e
kariukikinyanjui <michaelkariuki7@gmail.com>
2023-09-19 05:16:48 +0000

    Updated test

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -6,11 +6,24 @@ from models.base import Base
 
 class TestBase(unittest.TestCase):
 
-    def test_base_id(self):
+    def test_base_auto_assign_id(self):
         b1 = Base()
         b2 = Base()
         self.assertEqual(b1.id, 1)
         self.assertEqual(b2.id, 2)
+
+    def test_base_auto_increment_id(self):
+        b1 = Base()
+        b2 = Base()
+        b3 = Base()
+        self.assertEqual(b1.id, 1)
+        self.assertEqual(b2.id, 2)
+        self.assertEqual(b3.id, 3)
+
+    def test_base_save_id_passed(self):
+        b = Base(89)
+        self.assertEqual(b.id, 89)
+
 
 if __name__ == "__main__":
     unittest.main()
