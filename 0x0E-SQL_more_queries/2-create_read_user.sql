0d0c2b958db638e9990c3f0f78a53dc9bbaa643f
kariukikinyanjui <michaelkariuki7@gmail.com>
2023-10-19 01:15:24 +0000

    A script that creates a database and a user

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -2,7 +2,7 @@
 CREATE DATABASAE IF NOT EXISTS hbtn_0d_2;
 
 -- Create the user if it doesn't exist and set the password
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+CREATE USER IF NOT EXISTS user_0d_2@localhost IDENTIFIED BY 'user_0d_2_pwd';
 
 -- Grant SELECT  privilege to the user on the database
-GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
+GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost;
