b88be57743e2ea04fb27593d479c7f8f73b9bc3f
kariukikinyanjui <michaelkariuki7@gmail.com>
2023-11-13 09:14:57 +0000

    Wrote the variable in camel case

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,14 +1,9 @@
 #!/usr/bin/node
-const no_arguments = process.argv.length - 2;
-if (no_arguments === 0)
-{
-  console.log("No argument");
-}
-else if (no_arguments === 1)
-{
-  console.log("Argument found");
-}
-else
-{
-  console.log("Arguments found");
+const numArguments = process.argv.length - 2;
+if (numArguments === 0) {
+  console.log('No argument');
+} else if (numArguments === 1) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
 }
