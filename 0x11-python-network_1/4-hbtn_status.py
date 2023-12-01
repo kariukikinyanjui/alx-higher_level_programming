abca0278ba3f0148ab2d13b8d3109786bc3d8e9d
kariukikinyanjui <michaelkariuki7@gmail.com>
2023-12-01 23:37:25 +0000

    Fetch url using requests package

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Fetches URL using the requests package"""
+import requests
+
+url = 'https://alx-intranet.hbtn.io/status'
+
+response = requests.get(url)
+print("Body response:")
+print("\t- type:", type(response.text))
+print("\t- content:", response.text)
