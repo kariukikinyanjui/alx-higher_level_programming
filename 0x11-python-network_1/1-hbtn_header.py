90b9b20f08a79b5c5b166262852cb763e653871f
kariukikinyanjui <michaelkariuki7@gmail.com>
2023-12-01 20:33:17 +0000

    Checked if the correct number of arguments are provided

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -3,6 +3,9 @@
 
 import urllib.request
 import sys
+if len(sys.argv) != 2:
+    print("{} <URl>".format(sys.argv[0]))
+    sys.exit(1)
 
 url = sys.argv[1]
 with urllib.request.urlopen(url) as response:
