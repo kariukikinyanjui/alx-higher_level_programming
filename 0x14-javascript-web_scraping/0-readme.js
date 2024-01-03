cd6f3818002a704e8fe2a7b31e75c61e4de56f0d
kariukikinyanjui <michaelkariuki7@gmail.com>
2024-01-03 12:26:24 +0000

    Reads and prints the content of a file

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+// Import the File System Module
+const fs = require('fs');
+
+// Getting the file path from command line arguments
+const filePath = process.argv[2];
+
+// Read the content of the file in utf-8 encoding
+fs.readFile(filePath, 'utf-8', (err, data) => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log(data);
+  }
+});
