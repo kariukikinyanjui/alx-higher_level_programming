759e294f2390468fd38cffe6b9796ce9752c1f26
kariukinyanjui <michaelkariuki7@gmail.com>
2024-10-23 23:35:44 +0000

    print the ASCII alphabet in lowercase

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 for letter in range(97, 123):
-    if chr(letter) != 'q' and chr(letter) != 'e':
-        print("{}".format(chr(letter)), end="")
+    if letter in (101, 113):
+        continue
+    print("{}".format(chr(letter)), end="")
